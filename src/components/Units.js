cdf346e931e5ac5f15ab24f16efb0935abd3d6c8
deksss <deksss@i.ua>
2017-09-25 18:38:43 +0300

    Player Height fixed

--- src/components/Units.js
+++ src/components/Units.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 
 const style = {
-  minHeight: 200,
+  minHeight: 220,
   margin: "5px",
   padding: "5px",
   minWidth: 350,
@@ -21,7 +21,7 @@ export default class Units extends Component {
     const { items, renderItem, emptyMsg } = this.props;
     const itemsSorted = items.slice().sort((a, b) => b.index < a.index);
     const isEmpty = itemsSorted.length === 0;
-    style.height = this.props.height || style.height;
+    style.maxHeight = this.props.height || style.height;
 
     if (isEmpty) {
       return (
--- src/containers/Players.js
+++ src/containers/Players.js
@@ -201,7 +201,7 @@ class Players extends Component {
         <div style={{ display: "flex" }}>
           {admin && <UnitsToolbar addClick={this.handleAddPlayer} />}
           <Units
-            height={"100%"}
+            height={"50vh"}
             renderItem={this.renderUnit}
             items={items}
             emptyMsg={`\u00A0\u00A0\u00A0\u00A0 ( ͡° ͜ʖ ͡°)\u00A0\u00A0\u00A0\u00A0 (✪‿✪)ノ\u00A0\u00A0\u00A0\u00A0╚═( ͡° ͜ʖ ͡°)═╝\u00A0\u00A0\u00A0\u00A0 (∩^o^)⊃━☆ﾟ.*･｡ﾟ`}
